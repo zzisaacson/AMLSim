455f8e94d741c896db7d931cce7b396ef069c5c9
zachisaacson <zachary.isaacson@wustl.edu>
2020-06-01 19:54:42 -0400

    api conf params

--- API/app/routes/note_routes.js
+++ API/app/routes/note_routes.js
@@ -6,11 +6,26 @@ module.exports = function(app,db){
 		const http = require('http');
 		const url = require('url');
 
+		const fs = require('fs')
+		
 
+		let rawData =fs.readFileSync('../conf.json');
+		conf = JSON.parse(rawData);
+
+		if(url.parse(req.url,true).query.steps!=null){
+			conf.general.total_steps = Number(url.parse(req.url,true).query.steps)
+		}
+
+		if(url.parse(req.url,true).query.interval!=null){
+			conf.simulator.transaction_interval = Number(url.parse(req.url,true).query.interval)
+		}
+
+		fs.writeFileSync('../conf.json', JSON.stringify(conf, null, 2));
 
 		if(url.parse(req.url,true).query.num_transactions!=null){
 			numResults = Number(url.parse(req.url,true).query.num_transactions)
 		}
+
 		require("child_process").execSync("sh scripts/clean_logs.sh", {stdio: 'inherit',cwd: '..'});
 		require("child_process").execSync("python3 scripts/transaction_graph_generator.py conf.json", {stdio: 'inherit',cwd: '..'});
 		require("child_process").execSync("sh scripts/build_AMLSim.sh", {stdio: 'inherit',cwd: '..'});
@@ -19,9 +34,9 @@ module.exports = function(app,db){
 
 		
 
-		const neatCsv = require('neat-csv');
+		
 
-		const fs = require('fs')
+		const neatCsv = require('neat-csv');
 
 
 		fs.readFile('../outputs/test1/transactions.csv', async (err, data) => {
